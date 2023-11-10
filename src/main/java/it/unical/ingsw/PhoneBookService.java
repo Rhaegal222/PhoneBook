d67e647e7ad260d10fa9011333510835902ebf9b
Rhaegal222 <rhaegal222@gmail.com>
2023-11-10 12:13:30 +0100

    Update dependencies and add methods to PhoneBookRegistry and PhoneBookService classes

--- src/main/java/it/unical/ingsw/PhoneBookService.java
+++ src/main/java/it/unical/ingsw/PhoneBookService.java
@@ -1,2 +1,42 @@
-package it.unical.ingsw;public class PhoneBookService {
+package it.unical.ingsw;
+
+/**
+ * Questa classe rappresenta un servizio per la gestione di una rubrica telefonica.
+ * Fornisce metodi per verificare la presenza di un contatto, aggiungere un nuovo contatto
+ * e rimuovere un contatto esistente.
+ */
+public class PhoneBookService {
+    private final PhoneBookRegistry registry;
+
+    public PhoneBookService(PhoneBookRegistry registry) {
+        this.registry = registry;
+    }
+
+    /**
+     * Verifica se il nome del contatto è presente nella rubrica.
+     * @param name il nome del contatto da cercare
+     * @return true se il contatto è presente, false altrimenti
+     */
+    public boolean contains(String name) {
+        return registry.contains(name);
+    }
+
+    /**
+     * Aggiunge un nuovo contatto alla rubrica.
+     * @param name il nome del nuovo contatto
+     * @param number il numero di telefono del nuovo contatto
+     */
+    public void addContact(String name, String number) {
+        if(!registry.contains(name))
+            registry.addContact(name, number);
+    }
+
+    /**
+     * Rimuove un contatto esistente dalla rubrica.
+     * @param name il nome del contatto da rimuovere
+     */
+    public void removeContact(String name) {
+        if(registry.contains(name))
+            registry.removeContact(name);
+    }
 }
--- src/test/java/it/unical/ingsw/PhoneBookServiceTest.java
+++ src/test/java/it/unical/ingsw/PhoneBookServiceTest.java
@@ -1,20 +1,51 @@
 package it.unical.ingsw;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import org.junit.Test;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
- * Unit test for simple App.
+ * Questa classe contiene i test per la classe PhoneBookService.
+ * Viene utilizzato il framework Mockito per simulare il comportamento della classe PhoneBookRegistry.
  */
-public class AppTest 
-{
+class PhoneBookServiceTest {
+    @Mock
+    private PhoneBookRegistry registry;
+
+    private PhoneBookService service;
+
     /**
-     * Rigorous Test :-)
+     * Inizializza il framework Mockito e crea un'istanza di PhoneBookService.
+     */
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        service = new PhoneBookService(registry);
+    }
+
+    /**
+     * Verifica che il metodo addContact di PhoneBookService aggiunga un contatto correttamente.
+     * Viene simulato il comportamento del metodo contains di PhoneBookRegistry.
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void testAddContact() {
+        when(registry.contains("John")).thenReturn(false);
+        service.addContact("John", "1234567890");
+        verify(registry).addContact("John", "1234567890");
+    }
+
+    /**
+     * Verifica che il metodo removeContact di PhoneBookService rimuova un contatto correttamente.
+     * Viene simulato il comportamento del metodo contains di PhoneBookRegistry.
+     */
+    @Test
+    void testRemoveContact() {
+        when(registry.contains("John")).thenReturn(true);
+        service.removeContact("John");
+        verify(registry).removeContact("John");
     }
 }
